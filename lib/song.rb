3315f0e7b2bd620963eb2f55551175109c7756a5
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-09 17:42:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
   attr_accessor :name, :artist
+  @@all = []
 
   def initialize(name, artist = nil)
     @name = name
     @artist = artist
+    @@all << self
   end
 
   def artist_name
@@ -12,5 +14,9 @@ class Song
     else
       nil
     end
+  end
+  
+  def self.all
+    @@all
   end
 end
